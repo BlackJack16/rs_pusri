5512157477d25d555787090369673bd7b12e7fc6
priyanto <16priyanto@gmail.com>
2018-07-08 16:32:57 +0700

    only show dokter on today

--- pages/pasien/ambildokter.php
+++ pages/pasien/ambildokter.php
@@ -1,7 +1,7 @@
 <?php
 include "../../dist/koneksi.php"; 
 $idDokter = $_GET['idDokter'];
-$dokter= mysql_query("SELECT * FROM dokter where idDokter = '$idDokter'");
+$dokter= mysql_query("SELECT * FROM dokter inner JOIN jadwal on jadwal.idDokter = dokter.idDokter INNER join hari on jadwal.idHari = hari.idHari where hari.eng = (select DAYNAME(NOW())) and dokter.idDokter = '$idDokter' ORDER by hari.idHari");
 $getData = mysql_fetch_array($dokter);
 $spesialis = $getData['spesialis'];
 
@@ -10,8 +10,11 @@ echo '<div  class="form-group">
 <label>Dokter</label>
 <select name="idDokter" id="spesialis" class="form-control" required="">';
 
-while ($row = mysql_fetch_array($getAllSpesialis)) {
+if ($row = mysql_fetch_array($getAllSpesialis)) {
   echo "<option value=".$row['idDokter'].">".$row['nama']."</option>";
+}
+else {
+  echo "<option value=''>Tidak ada dokter pada hari ini</option>";
 }; ?>
 
  <input type="text" name="spesialis" value="<?php echo $spesialis; ?>" hidden>
